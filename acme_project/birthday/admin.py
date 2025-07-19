823b413b77debff9e2bdf42bdab11a2a78767d5d
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2025-07-19 12:34:51 +0500

    save codebase

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+
+
+from .models import Birthday
+
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday',
+    )
+
+    empty_value_display = 'Не задано'
+
+
+admin.site.register(Birthday, BirthdayAdmin)
